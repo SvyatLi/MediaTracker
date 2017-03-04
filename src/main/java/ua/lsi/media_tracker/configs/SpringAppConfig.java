7aefad5cd8a99cde51978615f600ed0f01d9d22b
LSI <sv9tli@gmail.com>
2017-03-04 23:40:18 +0200

    SQLite added, Spring Data added

--- src/main/java/ua/lsi/media_tracker/configs/SpringAppConfig.java
+++ src/main/java/ua/lsi/media_tracker/configs/SpringAppConfig.java
@@ -2,9 +2,13 @@ package ua.lsi.media_tracker.configs;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
+
+import javax.sql.DataSource;
 
 /**
  * Created by LSI on 07.04.2016.
@@ -23,11 +27,11 @@ public class SpringAppConfig {
         this.environment = environment;
     }
 
-//    @Bean
-//    public DataSource dataSource() {
-//        DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
-//        dataSourceBuilder.driverClassName("org.sqlite.JDBC");
-//        dataSourceBuilder.url("jdbc:sqlite:your.db");
-//        return dataSourceBuilder.build();
-//    }
+    @Bean
+    public DataSource dataSource() {
+        DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
+        dataSourceBuilder.driverClassName("org.sqlite.JDBC");
+        dataSourceBuilder.url("jdbc:sqlite:media_tracker.db");
+        return dataSourceBuilder.build();
+    }
 }
